d78665ffdcf6e209f1b2b4030d87206f2c21ac58
SidSan97 <sidnei1.8santiago@hotmail.com>
2023-09-25 17:37:24 -0300

    criando rotas

--- app/Http/Controllers/CadastroCliente.php
+++ app/Http/Controllers/CadastroCliente.php
@@ -9,27 +9,44 @@ class CadastroCliente extends Controller
 {
     public function cadastro(Request $request)
     {
-        $user = new User();
+        if($request->isMethod('post')) {
 
-        $user->nome     = $request->nome;
-        $user->email    = $request->email;
-        $user->senha = $request->senha;
-        $user->rg       = $request->rg;
-        $user->cpf_cnpj = $request->cpf_cnpj;
-        $user->data_nascimento = $request->data_nascimento;
-        $user->telefone = $request->telefone;
-        $user->estado_civil = $request->estado_civil;
-        $user->sexo         = $request->sexo;
-        $user->cep          = $request->cep;
-        $user->estado       = $request->estado;
-        $user->cidade       = $request->cidade;
-        $user->bairro       = $request->bairro;
-        $user->rua          = $request->rua;
-        $user->complemento  = $request->complemento;
+            $user = new User();
 
-        if($user->save())
-            dd('enviou');
-        else
-            dd('nao enviou');
+            $user->nome     = $request->nome;
+            $user->email    = $request->email;
+            $user->senha = $request->senha;
+            $user->rg       = $request->rg;
+            $user->cpf_cnpj = $request->cpf_cnpj;
+            $user->data_nascimento = $request->data_nascimento;
+            $user->telefone = $request->telefone;
+            $user->estado_civil = $request->estado_civil;
+            $user->sexo         = $request->sexo;
+            $user->cep          = $request->cep;
+            $user->estado       = $request->estado;
+            $user->cidade       = $request->cidade;
+            $user->bairro       = $request->bairro;
+            $user->rua          = $request->rua;
+            $user->complemento  = $request->complemento;
+
+            if($user->save()) {
+                return response()->json([
+                    "status"  => 201,
+                    "message" => "Cadastro feito com sucesso"
+                ], 201);
+
+            } else {
+                return response()->json([
+                    "status"  => 401,
+                    "message" => "Erro ao efetuar cadastro"
+                ], 401);
+            }
+
+        } else {
+            return response()->json([
+                "status"  => 405,
+                "message" => "Método não permitido. Use POST para cadastrar."
+            ], 405);
+        }
     }
 }
